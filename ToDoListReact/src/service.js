4700225fe2b962bc332f44bc4a81ef8fd93d9a0c
etti <etti0475@gmail.com>
2025-02-03 23:02:25 +0200

    my commit message

---
+++ ToDoListReact/src/service.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+
+const apiUrl = "http://localhost:5018"
+
+axios.defaults.baseURL = apiUrl;
+
+// הוספת interceptor לטיפול בשגיאות
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error('API Error:', error); // רישום השגיאה בלוג
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`${apiUrl}/items`)    
+    return result.data;
+  },
+
+  addTask: async(name) => {
+    console.log('addTask', name);
+    try {
+      const result = await axios.post(`/items`, { name, isComplete: false });
+      console.log(result.data.name);
+      return result.data;
+      
+    } catch (error) {
+      console.error('Error adding task:', error);
+      throw error;
+    }
+  },
+
+  setCompleted: async (id, name, isComplete) => {
+    console.log('setCompleted', { id, name, isComplete });
+    const updatedTask = { name, isComplete };
+    await axios.put(`/items/${id}`, updatedTask);
+    return { id, isComplete };
+  },
+
+
+
+  // setCompleted: async (id,name ,isComplete) => {
+  //   console.log('setCompleted', { id, isComplete,name });
+  //   debugger;
+    
+  //   try {
+      
+  //     const result = await axios.put(`/items/${id}`, { isComplete });
+  //     console.log(result.name);
+      
+  //     return result.data;
+  //   } catch (error) {
+  //     console.error('Error updating task:', error);
+  //     throw error;
+  //   }
+  // },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask');
+    try {
+      await axios.delete(`/items/${id}`);
+    } catch (error) {
+      console.error('Error deleting task:', error);
+      throw error;
+    }
+  }
+};
